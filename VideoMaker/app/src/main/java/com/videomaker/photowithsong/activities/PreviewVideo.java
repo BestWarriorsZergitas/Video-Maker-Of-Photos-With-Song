0435063190dee9ffeffb58381377af3d0f8d8a34
nguyenthihailoan <loannth@zergitas.com>
2017-08-17 23:40:04 +0700

    update

---
+++ VideoMaker/app/src/main/java/com/videomaker/photowithsong/activities/PreviewVideo.java
@@ -0,0 +1,15 @@
+package com.videomaker.photowithsong.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.videomaker.photowithsong.R;
+
+public class PreviewVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_video);
+    }
+}
