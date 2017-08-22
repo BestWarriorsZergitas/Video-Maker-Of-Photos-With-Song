44f4b63f72c2aace52c1859aa6aa6da91650e74b
nguyenthihailoan <loannth@zergitas.com>
2017-08-22 09:15:55 +0700

    22/08/2017

---
+++ TestMakeVideo/app/src/main/java/com/zergitas/testmakevideo/AsynTaskMakeVideo.java
@@ -0,0 +1,22 @@
+package com.zergitas.testmakevideo;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+/**
+ * Created by DaiPhongPC on 8/22/2017.
+ */
+
+public class AsynTaskMakeVideo extends AsyncTask<Void,Void,Void> {
+    ProgressDialog dialog;
+    FFmpeg recorder;
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
