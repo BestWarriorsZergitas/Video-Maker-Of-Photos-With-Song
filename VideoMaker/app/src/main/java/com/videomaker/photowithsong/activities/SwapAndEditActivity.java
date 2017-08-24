962bb269708d432cb2a4497603ff901eca5b89bd
DESKTOP-744HK17\Peih Gnaoh <hiepdnh@zergitas.com>
2017-08-24 09:24:04 +0700

    no message

--- VideoMaker/app/src/main/java/com/videomaker/photowithsong/activities/SwapAndEditActivity.java
+++ VideoMaker/app/src/main/java/com/videomaker/photowithsong/activities/SwapAndEditActivity.java
@@ -1,23 +1,19 @@
 package com.videomaker.photowithsong.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.mukesh.image_processing.ImageProcessor;
+import com.adobe.creativesdk.aviary.AdobeImageIntent;
 import com.videomaker.photowithsong.R;
 import com.videomaker.photowithsong.adapters.RecyclerListAdapter;
 import com.videomaker.photowithsong.helper.OnStartDragListener;
@@ -27,13 +23,6 @@ import com.videomaker.photowithsong.utils.Constant;
 
 import java.io.File;
 import java.util.ArrayList;
-
-import ly.img.android.sdk.models.constant.Directory;
-import ly.img.android.sdk.models.state.EditorLoadSettings;
-import ly.img.android.sdk.models.state.EditorSaveSettings;
-import ly.img.android.sdk.models.state.manager.SettingsList;
-import ly.img.android.ui.activities.ImgLyIntent;
-import ly.img.android.ui.activities.PhotoEditorBuilder;
 
 public class SwapAndEditActivity extends AppCompatActivity implements OnStartDragListener, RecyclerListAdapter.OnClickImageEdit {
 
@@ -66,7 +55,7 @@ public class SwapAndEditActivity extends AppCompatActivity implements OnStartDra
         mItemTouchHelper = new ItemTouchHelper(callback);
         mItemTouchHelper.attachToRecyclerView(recyclerView);
 
-        ivBack=(ImageView)findViewById(R.id.iv_back);
+        ivBack = (ImageView) findViewById(R.id.iv_back);
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,61 +81,13 @@ public class SwapAndEditActivity extends AppCompatActivity implements OnStartDra
     @Override
     public void onClickImageEdit(int position) {
         imageList = adapter.getmItems();
-        startEditor(imageList.get(position).getPath());
-    }
-
-    public void startEditor(String path) {
-        Log.e("TAG","ok");
-        String myPicture = path;
-        SettingsList settingsList = new SettingsList();
-        settingsList.getSettingsModel(EditorLoadSettings.class)
-                .setImageSourcePath(myPicture, true) // Load with delete protection true!
-
-                .getSettingsModel(EditorSaveSettings.class)
-                .setExportDir(Directory.DCIM, "")
-                .setExportPrefix("result_")
-                .setSavePolicy(
-                        EditorSaveSettings.SavePolicy.RETURN_ALWAYS_ONLY_OUTPUT
-                );
-        new PhotoEditorBuilder(this)
-                .setSettingsList(settingsList)
-                .startActivityForResult(this, CAMERA_PREVIEW_RESULT);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == CAMERA_PREVIEW_RESULT) {
-            String resultPath =
-                    data.getStringExtra(ImgLyIntent.RESULT_IMAGE_PATH);
-            String sourcePath =
-                    data.getStringExtra(ImgLyIntent.SOURCE_IMAGE_PATH);
-
-            if (resultPath != null) {
-                // Scan result file
-                File file =  new File(resultPath);
-                Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                Uri contentUri = Uri.fromFile(file);
-                scanIntent.setData(contentUri);
-                sendBroadcast(scanIntent);
-            }
-
-            if (sourcePath != null) {
-                // Scan camera file
-                File file =  new File(sourcePath);
-                Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                Uri contentUri = Uri.fromFile(file);
-                scanIntent.setData(contentUri);
-                sendBroadcast(scanIntent);
-            }
-            for (int i=0;i<imageList.size();i++){
-                if (imageList.get(i).getPath().equals(sourcePath)){
-                    imageList.get(i).setPath(resultPath);
-                    break;
-                }
-            }
-            adapter.notifyDataSetChanged();
-
-        }
+        Uri imageUri = Uri.fromFile(new File(imageList.get(position).getPath()));
+    /* 2) Create a new Intent */
+        Intent imageEditorIntent = new AdobeImageIntent.Builder(this)
+                .setData(imageUri)
+                .build();
+    /* 3) Start the Image Editor with request code 1 */
+        startActivityForResult(imageEditorIntent, 1);
     }
 
 }
--- VideoMaker/app/src/main/java/com/videomaker/photowithsong/aplication/Application.java
+++ VideoMaker/app/src/main/java/com/videomaker/photowithsong/aplication/Application.java
@@ -1,15 +1,43 @@
 package com.videomaker.photowithsong.aplication;
 
-import ly.img.android.PESDK;
+import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
+import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
+
 
 /**
  * Created by Peih Gnaoh on 8/22/2017.
  */
 
-public class Application extends android.app.Application {
+public class Application extends android.app.Application implements IAdobeAuthClientCredentials {
+    /* Be sure to fill in the two strings below. */
+    private static final String CREATIVE_SDK_CLIENT_ID = "<e9eb933510b04b9ab4c7f1286eb9259b>";
+    private static final String CREATIVE_SDK_CLIENT_SECRET = "<08412199-5eb6-4669-9cc3-331ccff9ca71>";
+    private static final String CREATIVE_SDK_REDIRECT_URI = "<ams+560554a81df45ef4ee3a62910e938052b1551fc2://adobeid/e9eb933510b04b9ab4c7f1286eb9259b>";
+    private static final String[] CREATIVE_SDK_SCOPES = {"email", "profile", "address"};
+
     @Override
     public void onCreate() {
         super.onCreate();
-        PESDK.init(this);
+        AdobeCSDKFoundation.initializeCSDKFoundation(getApplicationContext());
+    }
+
+    @Override
+    public String getClientID() {
+        return CREATIVE_SDK_CLIENT_ID;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String[] getAdditionalScopesList() {
+        return CREATIVE_SDK_SCOPES;
+    }
+
+    @Override
+    public String getRedirectURI() {
+        return CREATIVE_SDK_REDIRECT_URI;
     }
 }
